48db65d22e109d1384ec584388fa84cce4050ac0
Tung Bui Duy <tung.duy@tappcommerce.com>
2016-11-18 20:51:50 +0200

    migration

---
+++ db/migration/V1_002__add_status_to_task.sql
@@ -0,0 +1,17 @@
+create type task_status as enum ('done','progressing','canceled','blocked');
+
+alter table task add column status task_status not null  default 'progressing';
+
+create table comment (
+  comment_id bigserial primary key,
+  task_id uuid references task(task_id),
+  account_id bigint references account(account_id),
+  content text,
+  created_at timestamp with time zone not null default (current_timestamp),
+  modified_at timestamp with time zone
+);
+
+create trigger comment_modified_at_trigger
+before update on comment
+for each row
+execute procedure update_modified_at();
