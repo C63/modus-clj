d327db3c13b94062555456ff7a1cd9472621a740
Tung Bui Duy <tung.duy@tappcommerce.com>
2016-11-23 18:12:36 +0200

    whola docker

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,9 @@
 FROM java:8-alpine
 MAINTAINER TungDuy <tung.buiduy.91@gmail.com>
 
-ENV DB_CONNSTR jdbc:postgresql://modus-monolith-c63-dev.c8epalgxmd5k.eu-central-1.rds.amazonaws.com:5432/modus_dev
-ENV DB_USERNAME modus_c63_dev
-ENV DB_PASSWORD modus-c63-dev
 ADD target/modus-0.1.0-SNAPSHOT-standalone.jar /modus/modus-0.1.0-SNAPSHOT-standalone.jar
 
 EXPOSE 8080
 
-CMD ["java", "-jar", "/modus/modus-0.1.0-SNAPSHOT-standalone.jar"]
+ENTRYPOINT ["java", "-jar", "-DDB_CONNSTR=modus-monolith-c63-dev.c8epalgxmd5k.eu-central-1.rds.amazonaws.com:5432/modus_dev", "-DDB_USERNAME=modus_c63_dev", "-DDB_PASSWORD=modus-c63-dev"]
+CMD ["/modus/modus-0.1.0-SNAPSHOT-standalone.jar"]
--- src/modus/system/db_connection.clj
+++ src/modus/system/db_connection.clj
@@ -23,12 +23,10 @@
 
 (defn psql-db-properties
   []
-  (let [t {:url           (str "jdbc:postgresql:" (System/getProperty "DB_CONNSTR" "//localhost:5432/modus"))
-           :username      (System/getProperty "DB_USERNAME" (System/getProperty "user.name"))
-           :password      (System/getProperty "DB_PASSWORD" "")
-           :max-pool-size (int-system-property "DB_MAX_POOL_SIZE" 10)}]
-    (prn t)
-    t))
+  {:url           (str "jdbc:postgresql:" (System/getProperty "DB_CONNSTR" "//localhost:5432/modus"))
+   :username      (System/getProperty "DB_USERNAME" (System/getProperty "user.name"))
+   :password      (System/getProperty "DB_PASSWORD" "")
+   :max-pool-size (int-system-property "DB_MAX_POOL_SIZE" 10)})
 
 (defn datasource [db] (:singleton-datasource db))
 
